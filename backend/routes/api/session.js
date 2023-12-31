83d8761bbe59b984db3febf6fdb1c62303edf15e
Sydney Barnes-Grant <sydneybarnesg@gmail.com>
2023-12-31 14:07:43 -0500

    Add User input validation on user login backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -3,17 +3,30 @@ const express = require('express');
 const { Op } = require('sequelize');
 const bcrypt = require('bcryptjs');
 
-const { setTokenCookie, restoreUser } = require('../../utils/auth');
-const { User } = require('../../db/models');
-
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
+const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
 const router = express.Router();
 
+const validateLogin = [
+    check('credential')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a password.'),
+    handleValidationErrors
+  ];
+
+  
 // Log in
 router.post(
     '/',
+    validateLogin,
     async (req, res, next) => {
       const { credential, password } = req.body;
 
@@ -48,25 +61,14 @@ router.post(
     }
   );
 
-module.exports = router;
+// Log out
+router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+  );
 
 
-// // Log out
-// router.delete(
-//     '/',
-//     (_req, res) => {
-//       res.clearCookie('token');
-//       return res.json({ message: 'success' });
-//     }
-//   );
-
-//   const validateLogin = [
-//     check('credential')
-//       .exists({ checkFalsy: true })
-//       .notEmpty()
-//       .withMessage('Please provide a valid email or username.'),
-//     check('password')
-//       .exists({ checkFalsy: true })
-//       .withMessage('Please provide a password.'),
-//     handleValidationErrors
-//   ];
+  module.exports = router;
