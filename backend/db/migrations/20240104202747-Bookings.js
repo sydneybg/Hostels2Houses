35dac0333eee2df7a43471beadf3d4beb5520ff7
Sydney Barnes-Grant <sydneybarnesg@gmail.com>
2024-01-04 16:21:35 -0500

    Updated Bookings Migration and Models

--- backend/db/migrations/20240104202747-Bookings.js
+++ backend/db/migrations/20240104202747-Bookings.js
@@ -1,22 +1,52 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('Bookings', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      spotId: {
+        type: Sequelize.INTEGER,
+        references: { model: 'Spots' },
+        allowNull: false
+      },
+      guestId: {
+        type: Sequelize.INTEGER,
+        references: { model: 'Users' },
+        allowNull: false
+      },
+      startDate: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      endDate: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    options.tableName = "Bookings";
+    return queryInterface.dropTable('Bookings');
   }
 };
--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -10,7 +10,13 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Booking.belongsTo(models.Spot, {
+        foreignKey: 'spotId'
+      });
+
+      Booking.belongsTo(models.User, {
+        foreignKey: 'guestId'
+      });
     }
   }
   Booking.init({
