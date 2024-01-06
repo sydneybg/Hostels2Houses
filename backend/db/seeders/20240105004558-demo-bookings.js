874477ee27a536d5779c276df65ebed01647238f
Sydney Barnes-Grant <sydneybarnesg@gmail.com>
2024-01-06 17:48:13 -0500

    Trying second seeder file for booking

--- backend/db/seeders/20240105004558-demo-bookings.js
+++ backend/db/seeders/20240105004558-demo-bookings.js
@@ -1,32 +1,68 @@
+// 'use strict';
+
+// const { Booking, Spot, User } = require('../models');
+
+// let options = {};
+// if (process.env.NODE_ENV === 'production') {
+//   options.schema = process.env.SCHEMA;
+// };
+
+
+// module.exports = {
+//   async up (queryInterface, Sequelize) {
+//     options.tableName = 'Bookings';
+//     await Booking.bulkCreate(
+//       [
+//       {
+//         spotId: 1,
+//         guestId: 1,
+//         startDate: new Date('2025-06-01'),
+//         endDate: new Date('2025-06-10')
+//       }
+//     ], options);
+//   },
+
+//   async down (queryInterface, Sequelize) {
+//     options.tableName = 'Bookings';
+//     const Op = Sequelize.Op;
+//     return queryInterface.bulkDelete(options, {
+//       id: { [Op.gt]: 0 }
+//     });
+//   }
+// };
+
+
 'use strict';
 
-const { Booking, Spot, User } = require('../models');
+const { Booking } = require('../models')
 
 let options = {};
 if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;
-};
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
 
-
+/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
     options.tableName = 'Bookings';
-    await Booking.bulkCreate(
-      [
+    await Booking.bulkCreate(options,
+    [
       {
         spotId: 1,
-        guestId: 1,
-        startDate: new Date('2025-06-01'),
-        endDate: new Date('2025-06-10')
-      }
-    ], options);
+        userId: 1,
+        startDate: '2025-01-01',
+        endDate: '2025-01-03'
+      },
+
+
+    ], { validate: true })
   },
 
   async down (queryInterface, Sequelize) {
     options.tableName = 'Bookings';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      id: { [Op.gt]: 0 }
-    });
+      startDate: { [Op.in]: ['2022-01-01']}
+    }, {})
   }
 };
