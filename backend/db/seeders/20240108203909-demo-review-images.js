9b32ec73350764898247aebba989cada9cb59042
Sydney Barnes-Grant <sydneybarnesg@gmail.com>
2024-01-08 15:47:08 -0500

    Complete database with new migrations, models, and seeder files

---
+++ backend/db/seeders/20240108203909-demo-review-images.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const { ReviewImage } = require('../models');
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+};
+
+const demoReviewImages = [
+  {
+    reviewId: 1,
+    url: 'https://image1.jpg'
+  },
+  {
+    reviewId: 2,
+    url: 'https://image2.jpg'
+  },
+  {
+    reviewId: 3,
+    url: 'https://image3.jpg'
+  }
+];
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await ReviewImage.bulkCreate( demoReviewImages, options)
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'ReviewImages';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      url: { [Op.in]: ['www.abc.com'] }
+    }, {})
+  }
+};
---
+++ backend/db/seeders/20240108204348-demo-spot-images.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const { SpotImage } = require('../models');
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+};
+
+const demoSpotImages = [
+  {
+    spotId: 1,
+    url: "www.abc.com",
+    preview: true
+  },
+  {
+    spotId: 2,
+    url: "www.abcd.com"
+  },
+  {
+    spotId: 3,
+    url: "www.bdef.com",
+    preview: true
+  }
+]
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await SpotImage.bulkCreate( demoSpotImages, options)
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'SpotImage';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      url: { [Op.in]: ['www.abc.com']}
+    }, {})
+  }
+};
