587046c6421bfde1d14f9b6440503821050dab93
Sydney Barnes-Grant <sydneybarnesg@gmail.com>
2024-01-09 21:11:17 -0500

    Updated spots route for get all spots to include review and spot image but it is still including all review and spot image. Need to refactor to exclude unnecessary data. Also need to work on errors.

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -15,13 +15,72 @@ const router = express.Router();
 router.get(
     '/',
     async (req, res) => {
-        const spots = await Spot.findAll({include: [SpotImage , Review]})
-
-        //tell sql to also go into spotimage and get the associated ones
-        //get all reviews, isolate to starts, create average
+        let spots = await Spot.findAll({include: [SpotImage, Review]})
+        spots = spots.map(spot => {
+            const reviews = spot.Reviews
+            const numReviews = reviews.length
+            let sum = 0
+            reviews.forEach(review => {
+                sum += review.stars
+            });
+            const avgStars = sum / numReviews
+            spot.dataValues.avgStars = avgStars;
+            return spot
+        })
 
         return res.json(spots)
     }
 )
 
+
 module.exports = router;
+
+
+// router.get('/', async (req, res) => {
+
+//     const spots = await Spot.findAll({
+//       attributes: [
+//         'id',
+//         'ownerId',
+//         'address',
+//         'city',
+//         'state',
+//         'country',
+//         'lat',
+//         'lng',
+//         'name',
+//         'description',
+//         'price',
+//         'createdAt',
+//         'updatedAt'
+//       ],
+//       include: [{
+//         model: SpotImage,
+//         attributes: ['url'],
+//         where: {
+//           preview: true
+//         }
+//       }],
+//       raw: true,
+//       nest: true
+//     });
+
+//     // Calculate average ratings
+//     spots.forEach(spot => {
+//       // implementation code
+//     });
+
+//     // Shape response
+//     const formatted = {
+//       "Spots": spots.map(spot => {
+//         return {
+//           ...spot,
+//           "avgRating": spot.avgRating,
+//           "previewImage": spot.SpotImages[0]?.url
+//         };
+//       })
+//     };
+
+//     res.json(formatted);
+
+//   });
