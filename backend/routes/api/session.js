e52e6caee9e385b8d23f9f6dc20953cdaa73170b
Sydney Barnes-Grant <sydneybarnesg@gmail.com>
2024-01-10 10:12:52 -0500

    Fixed log-in 401 error

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -51,11 +51,9 @@ router.post(
       }
 
       if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
-        const err = new Error('Login failed');
-        err.status = 401;
-        err.title = 'Login failed';
-        err.errors = { credential: 'The provided credentials were invalid.' };
-        return next(err);
+        return res.status(401).json({
+          message: 'Invalid credentials',
+        })
       }
 
       const safeUser = {
@@ -102,3 +100,6 @@ router.get('/', (req, res) => {
 
 
   module.exports = router;
+
+
+
