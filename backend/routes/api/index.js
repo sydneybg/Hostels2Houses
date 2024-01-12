44d53d30afdd8ab47a7d55429c2a8808e1bba0ff
Sydney Barnes-Grant <sydneybarnesg@gmail.com>
2024-01-11 22:23:47 -0500

    Added a reviews router to the index

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,7 +2,7 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
-
+const reviewsRouter = require('./reviews.js')
 const spotsRouter = require('./spots.js');
 
 const { restoreUser } = require("../../utils/auth.js");
@@ -17,6 +17,8 @@ router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 
 router.use('/spots', spotsRouter);
+
+router.use('/reviews', reviewsRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
