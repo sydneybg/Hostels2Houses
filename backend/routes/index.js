ae0893efa03ee696f4ab99279c34ec4a072003be
Sydney Barnes-Grant <sydneybarnesg@gmail.com>
2024-02-12 12:53:42 -0500

    Continued

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,6 +3,15 @@ const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
 
+if (process.env.NODE_ENV !== 'production') {
+
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.json({});
+  });
+
+}
+
 router.use('/api', apiRouter);
 
 
