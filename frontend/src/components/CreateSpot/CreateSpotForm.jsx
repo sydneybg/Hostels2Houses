f0e1b1e2c4d3ffa3cadaa7fd99cb67009c90b2f9
Sydney Barnes-Grant <sydneybarnesg@gmail.com>
2024-02-22 20:02:28 -0500

    STill working on the form

--- frontend/src/components/CreateSpot/CreateSpotForm.jsx
+++ frontend/src/components/CreateSpot/CreateSpotForm.jsx
@@ -8,13 +8,43 @@ function CreateSpotForm() {
     const [country, setCountry] = useState('');const [state, setState] = useState('');
     const [streetAddress, setStreetAddress] = useState('');
     const [city, setCity] = useState('')
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [price, setPrice] = useState(null);
+    const [previewImage, setPreviewImage] = useState('');
+    const [images, setImages] = useState([]);
 
 
     const handleSubmit = (e) => {
         e.preventDefault();
         let errorsObj = {}
+
         if(!country) errorsObj.country = "Country is required"
-        setErrors(errorsObj)
+
+        if(!streetAddress) errorsObj.streetAddress = "Street Address is required"
+
+        if(!city) errors.city = "City is required";
+
+        if(!state) errors.state = "State is required";
+
+        if(description.length < 30) {
+          errors.description = "Description must be at least 30 characters";
+        }
+
+        if(!title) {
+          errors.title = "Title is required";
+        }
+
+        if(!price) {
+          errors.price = "Price per night is required";
+        }
+
+        if(!previewImage) {
+          errors.previewImage = "Preview Image URL is required";
+        }
+
+        setErrors(errors);
+
     }
 
     return (
@@ -40,6 +70,8 @@ function CreateSpotForm() {
                 onChange={(e) => setStreetAddress(e.target.value)}
                 />
             </label>
+            {errors.streetAddress && (<p>{errors.streetAddress}</p>)}
+
             <label>City
             <input
             type='text'
@@ -47,6 +79,8 @@ function CreateSpotForm() {
             onChange={(e) => setCity(e.target.value)}
             />
             </label>
+            {errors.city && <p>{errors.city}</p>}
+
             <label>State
                 <input
                 type='text'
@@ -54,15 +88,47 @@ function CreateSpotForm() {
                 onChange={(e) => setState(e.target.value)}
                 />
             </label>
+            {errors.state && <p>{errors.state}</p>}
 
             <h2>Describe your place to guests</h2>
             <caption>Mention the best features of your space, any special amentities like fast wifi or parking, and what you love about the neighborhood.</caption>
             <textarea placeholder='Please write at least 30 characters'></textarea>
+            {errors.description && <p>{errors.description}</p>}
+
+            <h2>Create a title for your spot</h2>
+      <p>Catch guests' attention with a spot title that highlights what makes your place special.</p>
+
+      <input
+        placeholder="Name of your spot"
+        value={title}
+        onChange={e => setTitle(e.target.value)}
+      />
+    {errors.title && <p>{errors.title}</p>}
+
+      <h2>Set a base price for your spot</h2>
+      <p>Competitive pricing can help your listing stand out and rank higher in search results.</p>
+
+      <input
+        type="number"
+        placeholder="Price per night (USD)"
+        value={price}
+        onChange={e => setPrice(e.target.value)}
+      />
+{errors.price && <p>{errors.price}</p>}
+
+
+      <h2>Liven up your spot with photos</h2>
+      <p>Submit a link to at least one photo to publish your spot.</p>
+
+      <input
+        placeholder="Preview Image URL"
+        value={previewImage}
+        onChange={e => setPreviewImage(e.target.value)}
+        required
+      />
 
         <button>Create Spot</button>
         </form>
-
-    <div>CreateSpotForm</div>
     </>
     )
 }
