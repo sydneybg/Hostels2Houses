3bd562c58fe0e4f872578365d3025c5e3c62d813
Sydney Barnes-Grant <sydneybarnesg@gmail.com>
2024-02-26 11:54:45 -0500

    Build to dist

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV === 'production') {
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
     return res.sendFile(
-      path.resolve(__dirname, '../../frontend', 'build', 'index.html') //change build to dist
+      path.resolve(__dirname, '../../frontend', 'dist', 'index.html') //change build to dist
     );
   });
 }
